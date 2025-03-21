3069a8f873582a5bca1fac81bbf79394684e0c2d
Venya <venyadurgam25@gmail.com>
2025-03-20 22:41:36 -0500

    Added search route functionality

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
 import tvRoutes from "./routes/tv.route.js";
+import searchRoutes from "./routes/search.route.js";
 
 import { ENV_VARS } from './config/envVars.js';
 import { connectDB } from './config/db.js';
@@ -16,6 +17,7 @@ app.use(cookieParser());
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie",protectRoute, movieRoutes);
 app.use("/api/v1/tv",protectRoute,tvRoutes);
+app.use("/api/v1/search",protectRoute,searchRoutes);
 
 // will allow us to parse req.body
 app.listen(PORT,() => {
