c3a4396d3a566cce247647dea65c37ef805eab86
Venya <venyadurgam25@gmail.com>
2025-03-21 21:05:13 -0500

    Updated App.js file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
-import './App.css'
+import './index.css'; 
 
 function App() {
   
-   return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+   return <h1 className='text-3xl font-bold underline text-red-600'>Hello world!</h1>;
 }
 
 export default App;
