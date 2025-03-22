11c0287b8d03f09fbcacb8ada762ef72dee9222d
Venya <venyadurgam25@gmail.com>
2025-03-21 21:18:20 -0500

    Created and updated Signup and login pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import { Route,Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
 
 function App() {
   
    return (
       <Routes>
          <Route path="/" element={<HomePage/>}/>
+         <Route path="/signup" element={<SignUpPage/>}/>
+         <Route path="/login" element={<LoginPage/>}/>
       </Routes>
    )
 }
