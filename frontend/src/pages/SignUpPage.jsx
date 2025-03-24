00fa4c82fd5e0195bff7864a28c6a76c89afdbaa
Venya <venyadurgam25@gmail.com>
2025-03-23 21:03:07 -0500

    Updated the state management for signup page

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { useState } from 'react';
 
 const SignUpPage = () => {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    console.log(email, username, password);
+  }
   return (
     <div className='h-screen w-full hero-bg'>
       <header className='max-w-6xl mx-auto flex items-center justify-between p-4 '>
@@ -14,27 +23,27 @@ const SignUpPage = () => {
           <h1 className="text-center text-white text-2xl font-bold mb-4">
             SignUp
           </h1>
-          <form className="space-y-4">
+          <form className="space-y-4" onSubmit={handleSignUp} >
             <div>
               <label htmlFor="email" className='text-sm font-medium text-gray-300 block'>
                 Email
               </label>
-              <input type="email" className='w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white focus:outline-none focus:ring' 
-              placeholder='you@example.com' id='email' />
+              <input type="email" className='w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white focus:outline-none focus:ring'
+                placeholder='you@example.com' id='email' value={email} onChange={(e) => setEmail(e.target.value)} />
             </div>
             <div>
               <label htmlFor="username" className='text-sm font-medium text-gray-300 block'>
                 Username
               </label>
-              <input type="text" className='w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white focus:outline-none focus:ring' 
-              placeholder='johndoe' id='username' />
+              <input type="text" className='w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white focus:outline-none focus:ring'
+                placeholder='johndoe' id='username' value={username} onChange={(e) => setUsername(e.target.value)} />
             </div>
             <div>
               <label htmlFor="password" className='text-sm font-medium text-gray-300 block'>
                 Password
               </label>
-              <input type="text" className='w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white focus:outline-none focus:ring' 
-              placeholder='..........' id='password' />
+              <input type="text" className='w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white focus:outline-none focus:ring'
+                placeholder='..........' id='password' value={password} onChange={(e) => setPassword(e.target.value)} />
             </div>
             <button className='w-full py-2 bg-red-600 text-white font-semibold rounded-md hover:bg-red-700 focus:outline-none focus:ring' type='submit'>
               Sign Up
@@ -50,4 +59,4 @@ const SignUpPage = () => {
   )
 }
 
-export default SignUpPage
+export default SignUpPage;
