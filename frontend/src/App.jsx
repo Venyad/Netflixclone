7227d9bd1c6f42a461b7cb5eb69ab409b167daa3
Venya <venyadurgam25@gmail.com>
2025-03-24 17:55:59 -0500

    Added footer component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import SignUpPage from "./pages/SignUpPage.jsx";
-
+import Footer from "./components/Footer.jsx";
 function App() {
    return (
-      <Routes>
+      <><Routes>
          <Route path="/" element={<HomePage />} />
          <Route path="/signup" element={<SignUpPage />} />
          <Route path="/login" element={<LoginPage />} />
-      </Routes>
+      </Routes><Footer /></>
    );
 }
 
