5965a8c4ec0d1fa00a8c302d583312771303ae30
Venya <venyadurgam25@gmail.com>
2025-03-26 23:57:09 -0500

    Updated Auth user file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,14 @@ import HomePage from "./pages/home/HomePage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import SignUpPage from "./pages/SignUpPage.jsx";
 import Footer from "./components/Footer.jsx";
+import { Toaster } from "react-hot-toast";
 function App() {
    return (
       <><Routes>
          <Route path="/" element={<HomePage />} />
          <Route path="/signup" element={<SignUpPage />} />
          <Route path="/login" element={<LoginPage />} />
-      </Routes><Footer /></>
+      </Routes><Footer /><Toaster /></>
    );
 }
 
--- frontend/src/store/authUser.js
+++ frontend/src/store/authUser.js
@@ -4,7 +4,18 @@ import { create } from "zustand";
 
 export const useAuthStore = create((set) => ({
     user: null,
-    signup:async() => {},
+    isSigningUp: false,
+    signup:async(credentials) => {
+        set({ isSigningUp: true });
+        try {
+			const response = await axios.post("/api/v1/auth/signup", credentials);
+			set({ user: response.data.user, isSigningUp: false});
+			toast.success("Account created successfully");
+		} catch (error) {
+			toast.error(error.response.data.message || "Signup failed");
+			set({ isSigningUp: false, user: null });
+		}
+    },
     login:async() => {},
     logout:async() => {},
     authCheck:async() => {},
