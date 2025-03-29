6a3c47f418632f3c672b5ab7f2e4a94403d35c89
Venya <venyadurgam25@gmail.com>
2025-03-28 23:42:30 -0500

    Updated Navbar component

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <header className='max-w-6xl mx-auto flex flex-wrap items-center justify-between p-4 h-20'>
+            <div className='flex items-center gap-10 z-50'>
+                <Link to="/">
+                    <img src='/netflix-logo.png' alt='Netflix Logo' className='w-32 sm:w-40' />
+                </Link>
+            </div>
+
+        </header>
+    )
+}
+
+export default Navbar
