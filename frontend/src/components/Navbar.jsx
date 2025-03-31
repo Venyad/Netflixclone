e9084e428f894755004bddbbf45a362109034f5a
Venya <venyadurgam25@gmail.com>
2025-03-30 22:42:41 -0500

    Updated navbar component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+    const [isMobileMenuOpen,setIsMobileMenuOpen] = useState(false);
+    const toggleMobileMenu = () => setIsMobileMenuOpen(!isMobileMenuOpen);
     return (
         <header className='max-w-6xl mx-auto flex flex-wrap items-center justify-between p-4 h-20'>
             <div className='flex items-center gap-10 z-50'>
@@ -20,6 +23,20 @@ const Navbar = () => {
                     </Link>
                 </div>
             </div>
+            {isMobileMenuOpen && (
+				<div className='w-full sm:hidden mt-4 z-50 bg-black border rounded border-gray-800'>
+					<Link to={"/"} className='block hover:underline p-2' onClick={toggleMobileMenu}>
+						Movies
+					</Link>
+					<Link to={"/"} className='block hover:underline p-2' onClick={toggleMobileMenu}>
+						Tv Shows
+					</Link>
+					<Link to={"/history"} className='block hover:underline p-2' onClick={toggleMobileMenu}>
+						Search History
+					</Link>
+				</div>
+			)}
+
 
         </header>
     )
