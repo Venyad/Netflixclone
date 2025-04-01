c7f8c70edb43d800218c98f5b6cb24cb107c8cc4
Venya <venyadurgam25@gmail.com>
2025-03-31 22:56:49 -0500

    Update UI for Home screen

--- frontend/src/pages/home/HomeScreen.jsx
+++ frontend/src/pages/home/HomeScreen.jsx
@@ -6,9 +6,13 @@ const HomeScreen = () => {
     <>
     <div className='relative h-screen text-white bg-black'>
       <Navbar/>
+      <img src="/extraction.jpg" alt="Hero img" className='absolute top-0 left-0 w-full h-full object-cover -z-50' />
+      <div className='absolute top-0 left-0 w-full h-full bg-black/50 -z-50' aria-hidden='true'></div>
+
     </div>
     </>
   );
 };
 
-export default HomeScreen
+export default HomeScreen;
+ 
