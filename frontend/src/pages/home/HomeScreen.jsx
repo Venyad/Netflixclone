f974c73a31ba602618ab4900f91fbbbb088a7caf
Venya <venyadurgam25@gmail.com>
2025-03-31 23:02:16 -0500

    Update UI for Home screen

--- frontend/src/pages/home/HomeScreen.jsx
+++ frontend/src/pages/home/HomeScreen.jsx
@@ -4,15 +4,22 @@ import Navbar from '../../components/Navbar.jsx'
 const HomeScreen = () => {
   return (
     <>
-    <div className='relative h-screen text-white bg-black'>
-      <Navbar/>
-      <img src="/extraction.jpg" alt="Hero img" className='absolute top-0 left-0 w-full h-full object-cover -z-50' />
-      <div className='absolute top-0 left-0 w-full h-full bg-black/50 -z-50' aria-hidden='true'></div>
+      <div className='relative h-screen text-white bg-black'>
+        <Navbar />
+        <img src="/extraction.jpg" alt="Hero img" className='absolute top-0 left-0 w-full h-full object-cover -z-50' />
+        <div className='absolute top-0 left-0 w-full h-full bg-black/50 -z-50' aria-hidden='true'></div>
+        <div className='absolute top-0 left-0 w-full h-full flex flex-col justify-center px-8 md:px-16 lg:px-32'>
+          <div
+            className='bg-gradient-to-b from-black via-transparent to-transparent 
+					absolute w-full h-full top-0 left-0 -z-10'
+          />
 
-    </div>
+        </div>
+
+
+      </div>
     </>
   );
 };
 
 export default HomeScreen;
- 
