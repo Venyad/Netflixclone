289196628867d6beecac0aa94bd030eed2da3ee2
Venya <venyadurgam25@gmail.com>
2025-04-02 22:49:58 -0500

    Updated Movie Slider component

--- frontend/src/components/MovieSlider.jsx
+++ frontend/src/components/MovieSlider.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useContentStore } from "../store/content.js";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { SMALL_IMG_BASE_URL } from "../utils/constants.js";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
-const MovieSlider = () => {
+const MovieSlider = ({ category }) => {
+  const { contentType } = useContentStore();
   return (
     <div>
-      
+      Movie Slider
     </div>
   )
 }
