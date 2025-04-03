acff3fe7e2604530a8a6229b3cb32738dd397c81
Venya <venyadurgam25@gmail.com>
2025-04-02 23:03:00 -0500

    Updated Movie Slider component

--- frontend/src/components/MovieSlider.jsx
+++ frontend/src/components/MovieSlider.jsx
@@ -8,24 +8,43 @@ import { ChevronLeft, ChevronRight } from "lucide-react";
 const MovieSlider = ({ category }) => {
   const { contentType } = useContentStore();
   const [content, setContent] = useState([]);
-	const [showArrows, setShowArrows] = useState(false);
+  const [showArrows, setShowArrows] = useState(false);
   const sliderRef = useRef(null);
 
-	const formattedCategoryName =
-		category.replaceAll("_", " ")[0].toUpperCase() + category.replaceAll("_", " ").slice(1);
-	const formattedContentType = contentType === "movie" ? "Movies" : "TV Shows";
+  const formattedCategoryName =
+    category.replaceAll("_", " ")[0].toUpperCase() + category.replaceAll("_", " ").slice(1);
+  const formattedContentType = contentType === "movie" ? "Movies" : "TV Shows";
   useEffect(() => {
-		const getContent = async () => {
-			const res = await axios.get(`/api/v1/${contentType}/${category}`);
-			setContent(res.data.content);
-		};
+    const getContent = async () => {
+      const res = await axios.get(`/api/v1/${contentType}/${category}`);
+      setContent(res.data.content);
+    };
 
-		getContent();
-	}, [contentType, category]);
+    getContent();
+  }, [contentType, category]);
 
   return (
-    <div>
-      Movie Slider
+    <div className='bg-black text-white relative px-5 md:px-20'
+      onMouseEnter={() => setShowArrows(true)}
+      onMouseLeave={() => setShowArrows(false)}>
+      <h2 className='mb-4 text-2xl font-bold'>
+        {formattedCategoryName} {formattedContentType}
+      </h2>
+      <div className='flex space-x-4 overflow-x-scroll scrollbar-hide' ref={sliderRef}>
+        {content.map((item) => (
+          <Link to={`/watch/${item.id}`} className='min-w-[250px] relative group' key={item.id}>
+            <div className='rounded-lg overflow-hidden'>
+              <img
+                src={SMALL_IMG_BASE_URL + item.backdrop_path}
+                alt='Movie image'
+                className='transition-transform duration-300 ease-in-out group-hover:scale-125'
+              />
+            </div>
+            <p className='mt-2 text-center'>{item.title || item.name}</p>
+          </Link>
+        ))}
+      </div>
+
     </div>
   )
 }
