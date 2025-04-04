193aa4da0fb12d7f79e2584a1de5dcc824bbdc3b
Venya <venyadurgam25@gmail.com>
2025-04-03 21:15:53 -0500

    Updated Watch Page component

--- frontend/src/pages/WatchPage.jsx
+++ frontend/src/pages/WatchPage.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import { useState } from 'react';
+import { useContentStore } from "../store/content.js";
 
 const WatchPage = () => {
-  return (
-    <div>
-      Watch
-    </div>
-  )
+    const { id } = useParams();
+    const [trailers, setTrailers] = useState([]);
+    const [currentTrailerIdx, setCurrentTrailerIdx] = useState(0);
+    const [loading, setLoading] = useState(true);
+    const [content, setContent] = useState({});
+    const [similarContent, setSimilarContent] = useState([]);
+    const { contentType } = useContentStore();
+    return (
+        <div>
+            Watch
+        </div>
+    )
 }
 
 export default WatchPage
