f20445088433f1b6da001c325aa1cd5a931e1534
Venya <venyadurgam25@gmail.com>
2025-04-03 21:12:03 -0500

    Created Watch Page component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import { useAuthStore } from "./store/authUser.js";
 import { useEffect } from "react";
 import { Navigate } from "react-router-dom";
 import { Loader } from "lucide-react";
+import WatchPage from "./pages/WatchPage.jsx";
 function App() {
    const {user,isCheckingAuth,authCheck} = useAuthStore();
    useEffect(() => {
@@ -27,6 +28,7 @@ function App() {
          <Route path="/" element={<HomePage />} />
          <Route path="/signup" element={!user ? <SignUpPage /> : <Navigate to={"/"} />} />
          <Route path="/login" element={!user ? <LoginPage /> : <Navigate to={"/"} />} />
+         <Route path="/watch/:id" element={user ? <WatchPage /> : <Navigate to={"/login"} />}/>
       </Routes><Footer /><Toaster /></>
    );
 }
---
+++ frontend/src/pages/WatchPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WatchPage = () => {
+  return (
+    <div>
+      Watch
+    </div>
+  )
+}
+
+export default WatchPage
