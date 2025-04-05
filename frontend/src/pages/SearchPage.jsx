bd5867b66396cc7c0d19230ad503ffe1631858fd
Venya <venyadurgam25@gmail.com>
2025-04-04 23:30:00 -0500

    Updated search page component

--- frontend/src/pages/SearchPage.jsx
+++ frontend/src/pages/SearchPage.jsx
@@ -11,15 +11,36 @@ import { Link } from "react-router-dom";
 
 const SearchPage = () => {
     const [activeTab, setActiveTab] = useState("movie");
-	const [searchTerm, setSearchTerm] = useState("");
+    const [searchTerm, setSearchTerm] = useState("");
 
-	const [results, setResults] = useState([]);
-	const { setContentType } = useContentStore();
-  return (
-    <div>
-      
-    </div>
-  )
+    const [results, setResults] = useState([]);
+    const { setContentType } = useContentStore();
+    return (
+        <div className='bg-black min-h-screen text-white'>
+            <Navbar />
+            <div className='container mx-auto px-4 py-8'>
+                <div className='flex justify-center gap-3 mb-4'>
+                    <button className={`py-2 px-4 rounded ${activeTab === "movie" ? "bg-red-600" : "bg-gray-800"
+                        } hover:bg-red-700`}>
+                        Movies
+                    </button>
+                    <button
+                        className={`py-2 px-4 rounded ${activeTab === "tv" ? "bg-red-600" : "bg-gray-800"
+                            } hover:bg-red-700`}
+                    >
+                        TV Shows
+                    </button>
+                    <button
+                        className={`py-2 px-4 rounded ${activeTab === "person" ? "bg-red-600" : "bg-gray-800"
+                            } hover:bg-red-700`}
+                    >
+                        Person
+                    </button>
+
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default SearchPage
