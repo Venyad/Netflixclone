387a064172a1a901d54d93f39ffeb89535f45bdd
Venya <venyadurgam25@gmail.com>
2025-04-04 23:26:24 -0500

    Updated search page component

--- frontend/src/pages/SearchPage.jsx
+++ frontend/src/pages/SearchPage.jsx
@@ -1,6 +1,20 @@
 import React from 'react'
+import { useState } from "react";
+import { useContentStore } from "../store/content.js";
+import Navbar from "../components/Navbar";
+import { Search } from "lucide-react";
+import toast from "react-hot-toast";
+import axios from "axios";
+import { ORIGINAL_IMG_BASE_URL } from "../utils/constants.js";
+import { Link } from "react-router-dom";
+
 
 const SearchPage = () => {
+    const [activeTab, setActiveTab] = useState("movie");
+	const [searchTerm, setSearchTerm] = useState("");
+
+	const [results, setResults] = useState([]);
+	const { setContentType } = useContentStore();
   return (
     <div>
       
