150b661632f7f19754276ec1408258dcc7e1fef2
Venya <venyadurgam25@gmail.com>
2025-04-05 21:30:59 -0500

    Updated Search History Page

--- frontend/src/pages/SearchHistoryPage.jsx
+++ frontend/src/pages/SearchHistoryPage.jsx
@@ -9,21 +9,42 @@ import toast from "react-hot-toast";
 const SearchHistoryPage = () => {
     const [searchHistory, setSearchHistory] = useState([]);
     useEffect(() => {
-		const getSearchHistory = async () => {
-			try {
-				const res = await axios.get(`/api/v1/search/history`);
-				setSearchHistory(res.data.content);
-			} catch (error) {
-				setSearchHistory([]);
-			}
-		};
-		getSearchHistory();
-	}, []);
-  return (
-    <div>
-      
-    </div>
-  )
+        const getSearchHistory = async () => {
+            try {
+                const res = await axios.get(`/api/v1/search/history`);
+                setSearchHistory(res.data.content);
+            } catch (error) {
+                setSearchHistory([]);
+            }
+        };
+        getSearchHistory();
+    }, []);
+    if (searchHistory?.length === 0) {
+		return (
+			<div className='bg-black min-h-screen text-white'>
+				<Navbar />
+				<div className='max-w-6xl mx-auto px-4 py-8'>
+					<h1 className='text-3xl font-bold mb-8'>Search History</h1>
+					<div className='flex justify-center items-center h-96'>
+						<p className='text-xl'>No search history found</p>
+					</div>
+				</div>
+			</div>
+		);
+	}
+    return (
+        <div className='bg-black text-white min-h-screen'>
+            <Navbar />
+            <div className='max-w-6xl mx-auto px-4 py-8'
+            >
+                <h1 className='text-3xl font-bold mb-8'>Search History</h1>
+                <div  className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3  gap-4'>
+
+                </div>
+            </div>
+
+        </div>
+    )
 }
 
 export default SearchHistoryPage
