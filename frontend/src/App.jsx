39753c55d4b24a7a35da1df0494c84cc5db44ad2
Venya <venyadurgam25@gmail.com>
2025-04-05 22:11:13 -0500

    Created 404 Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -33,6 +33,7 @@ function App() {
          <Route path="/watch/:id" element={user ? <WatchPage /> : <Navigate to={"/login"} />}/>
          <Route path='/search' element={user ? <SearchPage /> : <Navigate to={"/login"} />} />
          <Route path='/history' element={user ? <SearchHistoryPage /> : <Navigate to={"/login"} />} />
+         <Route path='/*' element={<NotFoundPage />} />
 
       </Routes><Footer /><Toaster /></>
    );
