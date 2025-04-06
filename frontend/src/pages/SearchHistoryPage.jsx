08e2aa869c5de6169a86db3bc71468e33dcf3733
Venya <venyadurgam25@gmail.com>
2025-04-05 21:26:11 -0500

    Updated Search History Page

--- frontend/src/pages/SearchHistoryPage.jsx
+++ frontend/src/pages/SearchHistoryPage.jsx
@@ -1,6 +1,24 @@
 import React from 'react'
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Navbar from "../components/Navbar.jsx";
+import { SMALL_IMG_BASE_URL } from "../utils/constants.js";
+import { Trash } from "lucide-react";
+import toast from "react-hot-toast";
 
 const SearchHistoryPage = () => {
+    const [searchHistory, setSearchHistory] = useState([]);
+    useEffect(() => {
+		const getSearchHistory = async () => {
+			try {
+				const res = await axios.get(`/api/v1/search/history`);
+				setSearchHistory(res.data.content);
+			} catch (error) {
+				setSearchHistory([]);
+			}
+		};
+		getSearchHistory();
+	}, []);
   return (
     <div>
       
