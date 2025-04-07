d7e01aeba4ac5e0eb282ff8ffa10b5c26d813d92
Venya <venyadurgam25@gmail.com>
2025-04-06 20:20:36 -0500

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
 import tvRoutes from "./routes/tv.route.js";
 import searchRoutes from "./routes/search.route.js";
+import path from "path";
+
 
 import { ENV_VARS } from './config/envVars.js';
 import { connectDB } from './config/db.js';
@@ -11,6 +13,7 @@ import { protectRoute } from './middleware/protectRoute.js';
 
 const app = express();
 const PORT = ENV_VARS.PORT;
+const __dirname = path.resolve();
 app.use(express.json()); 
 app.use(cookieParser());
 
@@ -19,6 +22,13 @@ app.use("/api/v1/movie",protectRoute, movieRoutes);
 app.use("/api/v1/tv",protectRoute,tvRoutes);
 app.use("/api/v1/search",protectRoute,searchRoutes);
 
+if (ENV_VARS.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 // will allow us to parse req.body
 app.listen(PORT,() => {
     console.log("Server stated at http://localhost:"+PORT);
